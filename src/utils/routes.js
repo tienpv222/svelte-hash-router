ecbd406bbd0e632d4afe7478adcebaf09b6160cc
pynnl <pynnl@outlook.com>
2019-06-13 23:09:59 -0400

    fix $$component on null

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -15,7 +15,8 @@ function parse (schema = {}, notRoot, pathname, href = '#') {
     let type = typeof schema
     schema = type === 'function' ? { $$component: schema }
       : type === 'string' ? { $$redirect: schema }
-      : type !== 'object' ? {} : schema
+      : type !== 'object' ? {}
+      : schema === null ? {} : schema
 
     let c = schema.$$component
     if (typeof c !== 'function' && c !== undefined && c !== null)
