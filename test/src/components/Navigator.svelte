dcacc0420d88cb993aec0e07c3d1c14eb8f91fb5
pynnl <pynnl@outlook.com>
2019-06-14 20:47:50 -0400

    .

--- test/src/components/Navigator.svelte
+++ test/src/components/Navigator.svelte
@@ -1,29 +1,40 @@
 <script>
-import { matches, active } from '../../../src'
-export let route = []
+import { active, matches } from '../../../src'
+export let route = {}
 export let deep = false
+export let exact = false
 export let pad = 0
+export let root = true
 
-$: routes = Array.isArray(route) ? route : Object.values(route)
-$: style = `padding-left:${++pad * 20}px`
+let routes = Array.isArray(route) ? route : Object.values(route)
+let style = `padding-left:${++pad * 20}px`
+let isActive = r => exact ? r === $active : $matches.includes(r)
 </script>
 
-{#each routes as e}
-  <a
-    class:active={e === $active} 
-    href={e.$$href}
-    {style}
-    > {e.$$pathname}
-  </a>
-  {#if deep}
-    <svelte:self route={e} root={false} {deep} {pad}/>
-  {/if}
-{/each}
-{#if !deep}
-  <div class='divider'></div>
+{#if root}
+  <div class='root'>
+    <svelte:self route={routes} {deep} root={false}/>
+  </div>
+{:else}
+  {#each routes as r}
+    <a
+      class:active={isActive(r)} 
+      href={r.$$href}
+      {style}
+      > {r.$$pathname}
+    </a>
+    {#if deep}
+      <svelte:self route={r} {deep} {pad} root={false}/>
+    {/if}
+  {/each}
 {/if}
 
 <style>
+.root {
+  float: left;
+  background: darkslategrey;
+}
+
 a {
   display: block;
   padding: 3px 20px;
--- test/src/index.js
+++ test/src/index.js
@@ -1,23 +1,7 @@
 import { routes, Router } from '../../src'
-import App from './App.svelte'
-import Basic, { basicRoutes } from './views/Basic.svelte'
-import Nested, { nestedRoutes } from './views/Nested.svelte'
-import Wildcard, { wildcardRoutes } from './views/Wildcard.svelte'
-import Params, { paramsRoutes } from './views/Params.svelte'
-import Query from './views/Query.svelte'
-import Redirect from './views/Redirect.svelte'
 
 routes.set({
-  '/': {
-    $$component: App,
-    'basic': { $$component: Basic, ...basicRoutes },
-    'nested': { $$component: Nested, ...nestedRoutes },
-    'wildcard': { $$component: Wildcard, ...wildcardRoutes },
-    'params': { $$component: Params, ...paramsRoutes },
-    'query': Query,
-    'redirect': Redirect
-  },
-  '*': '/redirect'
+  '/': 
 })
 
 let app = new Router({
